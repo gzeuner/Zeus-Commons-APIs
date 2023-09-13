8a398db185c27e6d756384d6696364cba2328f98
Guido Zeuner <gzeuner@tiny-tool.de>
2023-09-13 18:20:47 +0200

    Adjusted Parameter Settings in main.

--- src/main/java/de/zeus/commons/provider/Provider.java
+++ src/main/java/de/zeus/commons/provider/Provider.java
@@ -17,90 +17,95 @@ import org.apache.commons.logging.LogFactory;
  */
 public class Provider {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		if (args.length == 3 || args.length == 4) {
-			ProviderConfig config = parseArgs(args);
-			Provider.initConfig(config);
-			ConnectionControllerFactory controllerFactory = new ConnectionControllerFactory();
+        ProviderConfig config = parseArgs(args);
+        Provider.initConfig(config);
+        ConnectionControllerFactory controllerFactory = new ConnectionControllerFactory();
 
-			if (config.getMode() != null) {
-				initServiceFromConsole(controllerFactory, config.getJsonRequest(), config.getMode());
-			} else {
-				initService(controllerFactory);
-			}
+        if (config.getMode() != null) {
+            initServiceFromConsole(controllerFactory, config.getJsonRequest(), config.getMode());
+        } else {
+            initService(controllerFactory);
+        }
 
-		} else {
-			displayUsage();
-			System.exit(0);
-		}
-	}
-	public static ProviderConfig parseArgs(String[] args) {
-		ProviderConfig config = new ProviderConfig();
+    }
 
-		String runMode = args[0];
+    public static ProviderConfig parseArgs(String[] args) {
 
-		if(runMode.equals("REST")) {
-			config.setJdbcConfig(args[1]);
-			config.setSparkConfig(args[2]);
-		}
-		else {
-			config.setJdbcConfig(args[1]);
-			config.setJsonRequest(new Gson().fromJson(args[3], JsonObject.class));
-		}
+        ProviderConfig config = new ProviderConfig();
+        try {
+            String runMode = args[0];
+            if (runMode.equals("REST")) {
+                config.setJdbcConfig(args[1]);
+                config.setSparkConfig(args[2]);
+            } else if (runMode.equals("CONSOLE")) {
+                config.setJdbcConfig(args[1]);
+                config.setSparkConfig(args[2]);
+                config.setJsonRequest(new Gson().fromJson(args[3], JsonObject.class));
+                config.setMode(args[4]);
+            } else {
+                displayUsage();
+                System.exit(0);
+            }
+        }catch (ArrayIndexOutOfBoundsException e) {
+            displayUsage();
+            System.exit(0);
+        }
 
-		return config;
-	}
+        return config;
+    }
 
-	/**
-	 * Displays the usage information for running the application.
-	 */
-	public static void displayUsage() {
-		System.out.println("Usage:");
-		System.out.println("============================================");
-		System.out.println("Run Service as REST-Service");
-		System.out.println("============================================");
-		System.out.println("Parm 1: jdbc-properties-file");
-		System.out.println("Parm 2: spark-properties-file");
-		System.out.println("Parm 3: log4j-config-file");
-		System.out.println("============================================");
-		System.out.println("Run Service from console");
-		System.out.println("============================================");
-		System.out.println("Parm 1: application-properties-file");
-		System.out.println("Parm 2: log4j-config-file");
-		System.out.println("Parm 3: application/xml or application/json");
-		System.out.println("Parm 4: JSON Query-Objects {}");
-	}
+    /**
+     * Displays the usage information for running the application.
+     */
+    public static void displayUsage() {
+        System.out.println("Usage:");
+        System.out.println("============================================");
+        System.out.println("Run Service as REST-Service");
+        System.out.println("============================================");
+        System.out.println("Parm 1: REST");
+        System.out.println("Parm 2: <jdbc-properties-file>");
+        System.out.println("Parm 3: <spark-properties-file>");
+        System.out.println("============================================");
+        System.out.println("Run Service from console");
+        System.out.println("============================================");
+        System.out.println("Parm 1: CONSOLE");
+        System.out.println("Parm 2: <jdbc-properties-file>");
+        System.out.println("Parm 3: <JSON Query-Objects {}>");
+        System.out.println("Parm 4: <application/xml or application/json>");
+    }
 
-	/**
-	 * Initializes and starts the service as a REST service.
-	 *
-	 * @param controllerFactory The factory for creating connection controllers.
-	 */
-	public static void initService(ConnectionControllerFactory controllerFactory) {
-		new HttpServer(controllerFactory);
-	}
+    /**
+     * Initializes and starts the service as a REST service.
+     *
+     * @param controllerFactory The factory for creating connection controllers.
+     */
+    public static void initService(ConnectionControllerFactory controllerFactory) {
+        new HttpServer(controllerFactory);
+    }
 
-	/**
-	 * Initializes and runs the service from the console with the specified JSON request and processing mode.
-	 *
-	 * @param controllerFactory The factory for creating connection controllers.
-	 * @param jsonRequest       The JSON request to process.
-	 * @param mode              The processing mode (JSON or XML).
-	 */
-	public static void initServiceFromConsole(ConnectionControllerFactory controllerFactory, JsonObject jsonRequest, String mode) {
-		IConnectionController connectionController = controllerFactory.getController();
-		System.out.println(connectionController.process(jsonRequest, mode));
-	}
+    /**
+     * Initializes and runs the service from the console with the specified JSON request and processing mode.
+     *
+     * @param controllerFactory The factory for creating connection controllers.
+     * @param jsonRequest       The JSON request to process.
+     * @param mode              The processing mode (JSON or XML).
+     */
+    public static void initServiceFromConsole(ConnectionControllerFactory controllerFactory, JsonObject jsonRequest, String mode) {
+        IConnectionController connectionController = controllerFactory.getController();
+        System.out.println(connectionController.process(jsonRequest, mode));
+    }
 
-	private static void initConfig(ProviderConfig config) {
+    private static void initConfig(ProviderConfig config) {
 
-		JdbcConfig jdbcConfig = JdbcConfig.getInstance();
-		jdbcConfig.setPropertiesFile(config.getJdbcConfig());;
-		jdbcConfig.loadProperties();
+        JdbcConfig jdbcConfig = JdbcConfig.getInstance();
+        jdbcConfig.setPropertiesFile(config.getJdbcConfig());
+        ;
+        jdbcConfig.loadProperties();
 
-		SparkConfig sparkConfig = SparkConfig.getInstance();
-		sparkConfig.setPropertiesFile(config.getSparkConfig());
-		sparkConfig.loadProperties();
-	}
+        SparkConfig sparkConfig = SparkConfig.getInstance();
+        sparkConfig.setPropertiesFile(config.getSparkConfig());
+        sparkConfig.loadProperties();
+    }
 }
